355210e45d00a5327b7a41c9582ee31167d79b69
Enrico Zannini <enricozannini93@gmail.com>
2019-10-26 10:35:48 +0200

    Initial Commit

---
+++ EZInteractiveNavigationSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EZInteractiveNavigationSample
+//
+//  Created by Enrico Zannini on 26/10/2019.
+//  Copyright © 2019 Enrico Zannini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
