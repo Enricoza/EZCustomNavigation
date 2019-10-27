c8a42ac156774c75a35edce4c14e052cbd547473
Enrico Zannini <enricozannini93@gmail.com>
2019-10-27 11:06:06 +0100

    Change the storyboard to test with child view controllers

---
+++ EZInteractiveNavigationSample/ChildNavigationController.swift
@@ -0,0 +1,24 @@
+//
+//  ChildNavigationController.swift
+//  EZInteractiveNavigationSample
+//
+//  Created by Enrico Zannini on 27/10/2019.
+//  Copyright © 2019 Enrico Zannini. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class ChildNavigationController: EZNavigationController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationBar.isHidden = true
+        
+        
+    }
+}
