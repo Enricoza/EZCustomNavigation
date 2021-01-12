2e48c5fa617992bdf86b04318e8f79b27420c69f
Serg Buglakov <sbuglakov@xiag.ch>
2021-01-12 11:34:14 +0700

    demo bug

---
+++ Example/EZInteractiveNavigationSample/tableviewcontroller.swift
@@ -0,0 +1,30 @@
+//
+//  tableviewcontroller.swift
+//  EZInteractiveNavigationSample
+//
+//  Created by Serg Buglakov on 12.01.2021.
+//  Copyright © 2021 Enrico Zannini. All rights reserved.
+//
+
+import UIKit
+
+class TableDemo: UITableViewController {
+    var n: Int = 10
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return n
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            n -= 1
+        }
+    }
+}
